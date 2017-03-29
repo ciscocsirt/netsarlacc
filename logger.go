dc784c257d12ec2df2a9c2af19aa105003a8b748
Josh eddy <joeddy@cisco.com>
2017-03-29 09:36:43 -0400

    Update logger.go

--- logger.go
+++ logger.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-	"gopkg.in/natefinch/lumberjack.v2"
-	"log"
-	"log/syslog"
-	"fmt"
+        "gopkg.in/natefinch/lumberjack.v2"
+        "log"
+        "log/syslog"
+        "fmt"
 )
 
 func AppLogger(err error) {
-	logwriter, e := syslog.New(syslog.LOG_CRIT, "netsarlacc")
-	if e == nil {
-		log.SetOutput(logwriter)
-	}
-	log.Print(err)
+        logwriter, e := syslog.New(syslog.LOG_CRIT, "netsarlacc")
+        if e == nil {
+                log.SetOutput(logwriter)
+        }
+        log.Print(err)
 }
 
 func ConnLogger(v interface{}) {
-	log.SetOutput(&lumberjack.Logger{
-		Filename: "netsarlacc.log",
-		MaxSize:  1, // megabytes
-	})
-	log.Println(v)
+        log.SetOutput(&lumberjack.Logger{
+                Filename: "netsarlacc.log",
+                MaxSize:  1, // megabytes
+        })
+        log.Println(v)
 }
 
-func writeLogger(logChan chan string) {
-	s := <-logChan
-	fmt.Println(s)
+func writeLogger(Logchan chan string) {
+        for v := range Logchan {
+        fmt.Println(v)
+        }
 }
-	
