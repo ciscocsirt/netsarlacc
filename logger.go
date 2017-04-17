66bb521b85502d0096fc68d45ec659fffadfb2b6
Josh eddy <joeddy@cisco.com>
2017-04-17 09:58:06 -0400

    Update logger.go

--- logger.go
+++ logger.go
@@ -27,24 +27,44 @@ func ConnLogger(v interface{}) {
 }
 
 func writeLogger(Logchan chan string) {
+        //variables
         var logFile *os.File
+        var writeFile bool = true
         
         //get current datetime and creates filename based on current time
         now := time.Now()
         filename := ("sinkhole-" + now.Format("2006-01-02-15-04-05") + ".log")
         
-        //create file
+        //create inital file
         logFile, err = os.OpenFile(filename, os.O_CREATE|os.O_RDWR, 0666)
         if err != nil {
                 log.Fatal(err)
         }
         
+        //ticker and file rotation goroutine
+        ticker := time.NewTicker(time.Minute * 10)
+        go func() {
+                for t := range ticker.C {
+                        writeFile = false
+                        logfile.Close()
+                        filename := ("sinkhole-" + now.Format("2006-01-02-15-04-05") + ".log")
+                        logFile, err = os.OpenFile(filename, os.O_CREATE|os.O_RDWR, 0666)
+                        if err != nil {
+                                log.Fatal(err)
+                        }
+                        writeFile = true
+                }
+        }
+                        
+        
         defer logfile.Close()
 
         for l := range Logchan {
-                n, err := io.WriteString(logFile, l + "\n")
-                if err != nil {
-                        fmt.Println(n, err)
+                if writeFile = true{
+                        n, err := io.WriteString(logFile, l + "\n")
+                        if err != nil {
+                                fmt.Println(n, err)
+                        }
                 }
         }
 }
