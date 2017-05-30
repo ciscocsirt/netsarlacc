f0d8a5d9b18767825a3fb0386bacd071326722ed
bmenrigh <dontspam@brandonenright.net>
2017-05-30 22:15:47 +0000

    Fixed race condition and other threading pitfalls
    The use of a boolean write variable isn't safe with threading. The
    value could be changed by the goroutine after the if statement was
    already checked.  Also, when the goroutine marked the writer as false,
    the range Logchan log writing code didn't have a provision to wait for
    it to be true so logs would get dropped while the file was being
    changed by the goroutine.
    
    Instead I've used a mutex which provides the necessary synchronization
    so that nothing can get lost and there aren't any race conditions.

--- logger.go
+++ logger.go
@@ -7,6 +7,7 @@ import (
         "os"
         "io"
         "time"
+	"sync"
 )
 
 func AppLogger(err error) {
@@ -20,23 +21,26 @@ func AppLogger(err error) {
 func writeLogger(Logchan chan string) {
         //variables
         var logFile *os.File
-        var writeFile bool = true
 	var err error
+	var newfilemutex = &sync.Mutex{}
 
         //ticker and file rotation goroutine
         ticker := time.NewTicker(time.Minute * 10)
         go func() {
         	//get current datetime and creates filename based on current time
+		newfilemutex.Lock()
         	now := time.Now()
         	filename := ("sinkhole-" + now.Format("2006-01-02-15-04-05") + ".log")
 
         	//create inital file
         	logFile, err = os.OpenFile(filename, os.O_CREATE|os.O_RDWR, 0666)
+		newfilemutex.Unlock()
         	if err != nil {
                 	log.Fatal(err)
        		}
+
                 for range ticker.C {
-                        writeFile = false
+			newfilemutex.Lock()
                         logFile.Close()
 			//get current datetime and creates filename based on current time
         		now := time.Now()
@@ -45,7 +49,7 @@ func writeLogger(Logchan chan string) {
                         if err != nil {
                                 log.Fatal(err)
                         }
-                        writeFile = true
+			newfilemutex.Unlock()
                 }
         }()
 
@@ -53,11 +57,12 @@ func writeLogger(Logchan chan string) {
         defer logFile.Close()
 
         for l := range Logchan {
-                if writeFile == true{
-                        n, err := io.WriteString(logFile, l + "\n")
-                        if err != nil {
-                                fmt.Println(n, err)
-                        }
-                }
-        }
+		newfilemutex.Lock()
+		n, err := io.WriteString(logFile, l + "\n")
+		if err != nil {
+			fmt.Println(n, err)
+		}
+		newfilemutex.Unlock()
+	}
+
 }
